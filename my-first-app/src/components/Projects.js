614d46fc88357716b55569eab6055fc946d0b301
Miqueas <miqueas.moreno2016@gmail.com>
2022-02-22 22:29:28 -0300

    Base de Project hecho

--- my-first-app/src/components/Projects.js
+++ my-first-app/src/components/Projects.js
@@ -1,7 +1,61 @@
 function Projects () {
     return (
         <section className="projectContainer">
-            Proyecto container ahr
+            <div className="projectText">
+                <h2>Projects</h2>
+            </div>
+            <div className="projectSliderC">
+                <div className="projectTemplate">
+                    <p>Imagen</p>
+                    <h4>Titulo</h4>
+                    <p>Descripcion</p>
+                </div>
+                <div className="projectTemplate">
+                    <p>Imagen</p>
+                    <h4>Titulo</h4>
+                    <p>Descripcion</p>
+                </div>
+                <div className="projectTemplate">
+                    <p>Imagen</p>
+                    <h4>Titulo</h4>
+                    <p>Descripcion</p>
+                </div>
+                <div className="projectTemplate">
+                    <p>Imagen</p>
+                    <h4>Titulo</h4>
+                    <p>Descripcion</p>
+                </div>
+                <div className="projectTemplate">
+                    <p>Imagen</p>
+                    <h4>Titulo</h4>
+                    <p>Descripcion</p>
+                </div>
+                <div className="projectTemplate">
+                    <p>Imagen</p>
+                    <h4>Titulo</h4>
+                    <p>Descripcion</p>
+                </div>
+                <div className="projectTemplate">
+                    <p>Imagen</p>
+                    <h4>Titulo</h4>
+                    <p>Descripcion</p>
+                </div>
+                <div className="projectTemplate">
+                    <p>Imagen</p>
+                    <h4>Titulo</h4>
+                    <p>Descripcion</p>
+                </div>
+                <div className="projectTemplate">
+                    <p>Imagen</p>
+                    <h4>Titulo</h4>
+                    <p>Descripcion</p>
+                </div>
+                <div className="projectTemplate">
+                    <p>Imagen</p>
+                    <h4>Titulo</h4>
+                    <p>Descripcion</p>
+                </div>
+            </div>
         </section>
     )
 }
