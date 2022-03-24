e3aece130a554ff8c9d707e9b3959842967d1625
Miqueas <miqueas.moreno2016@gmail.com>
2022-03-23 22:20:37 -0300

    El videobackground de Home se pausa cuando se sale del 50% de la seccion de Home, aun faltan añadir mas funcionalidades

--- my-first-app/src/components/Home.jsx
+++ my-first-app/src/components/Home.jsx
@@ -1,12 +1,49 @@
-import React, { Component } from "react";
+import React, { useEffect, useRef, useMemo } from "react";
 
-class Home extends Component {
-    render() {
+
+
+function Home() {
+
+    
+    //Pausar el video si la seccion 'Home' no aparece
+    const homeVideo = useRef();
+    const homeContainer = useRef();
+
+    const callbackFunction = entries => {
+        const [entry] = entries;
+        if (entry.isIntersecting) {
+            homeVideo.current.play();
+        } else {
+            homeVideo.current.pause();
+        }
+    }
+
+    const options = useMemo(() => {
+        return {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.5,
+        }
+    }, []);
+
+    useEffect(() => {
+        // max-width: 765px
+        //const matchMotion = window.matchMedia('(prefers-reduced-motion: reduce)');
+        const observeHome = new IntersectionObserver(callbackFunction, options);
+        const currentHome = homeContainer.current;
+        // const currentVideo = homeVideo.current;
+        if (currentHome) observeHome.observe(currentHome);
+
+        return () => {
+            if (currentHome) observeHome.unobserve(currentHome);
+        }
+    }, [homeContainer, options])
+
         return (
-            <section id="1" className="homeContainer">
+            <section id="1" ref={homeContainer} className="homeContainer">
 
                 <div className="homeVideoContainer">
-                    <video id="homeVideo" className="homeVideo" muted loop autoPlay playsInline poster="/video/load-video-min">
+                    <video ref={homeVideo} className="homeVideo" muted loop autoPlay playsInline poster="./img/load-video-min.png">
                     <source src="/video/Sphere-5.mp4" type="video/mp4"/>
                     </video>
                 </div>
@@ -20,7 +57,6 @@ class Home extends Component {
                 
             </section>
         )
-    }
 }
 
 export default Home;
